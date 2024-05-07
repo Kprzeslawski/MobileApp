ef4b27415de4cb6361d9b2e07105070007bf5085
Krzysztof <kprzeslawski01@gmail.com>
2024-05-07 23:06:18 +0200

    added stats for player and enemy

---
+++ app/src/main/java/com/example/app1/dataStorage/dataTypes/Stats.java
@@ -0,0 +1,35 @@
+package com.example.app1.dataStorage.dataTypes;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class Stats {
+
+    @Builder.Default
+    Integer health = 0;
+
+    @Builder.Default
+    Integer attack_dmg = 0;
+
+    @Builder.Default
+    Integer armor = 0;
+
+    @Builder.Default
+    Integer def = 0;
+
+    @Builder.Default
+    Integer pow = 0;
+
+    @Builder.Default
+    Integer agile = 0;
+
+    @Builder.Default
+    Integer c_rate = 0;
+
+    @Builder.Default
+    Integer c_dmg = 0;
+}
