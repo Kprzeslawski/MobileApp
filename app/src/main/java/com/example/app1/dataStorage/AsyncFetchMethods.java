f0f39b73447b7a283b7014ed3adb0bfb58fb8148
Krzysztof <kprzeslawski01@gmail.com>
2024-05-07 23:13:45 +0200

    request for player inventory

--- app/src/main/java/com/example/app1/dataStorage/AsyncFetchMethods.java
+++ app/src/main/java/com/example/app1/dataStorage/AsyncFetchMethods.java
@@ -1,5 +1,6 @@
 package com.example.app1.dataStorage;
 
+import com.example.app1.dataStorage.dataTypes.InventoryResponse;
 import com.example.app1.dataStorage.dataTypes.Location;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -77,6 +78,45 @@ public class AsyncFetchMethods {
                         ObjectMapper objectMapper = new ObjectMapper();
                         List<Location> locationList = objectMapper.readValue(response.toString(), new TypeReference<List<Location>>() {});
                         locationList.forEach(o -> System.out.println("NAME: " + o.getName()));
+                    } else {
+                        System.out.println("Failed to fetch data. Status code: " + responseCode);
+                    }
+                    connection.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    public static void fetchPlayerInventory(String playerId){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(baseConnection + "/player/player_inventory/" + playerId);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.connect();
+
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        String inputLine;
+                        StringBuilder response = new StringBuilder();
+
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+                        in.close();
+
+                        String jsonResponse = response.toString();
+
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        InventoryResponse playerInventory = objectMapper.readValue(response.toString(), InventoryResponse.class);
+                        System.out.println("GOLD: " + playerInventory.getGold());
+                        System.out.println("N_ITEMS: " + playerInventory.getItems().size());
                     } else {
                         System.out.println("Failed to fetch data. Status code: " + responseCode);
                     }
