4230ad8cfd1a930c7529942cb36c1cbf1e5b8f97
Krzysztof <kprzeslawski01@gmail.com>
2024-05-11 10:38:07 +0200

    adding elements dynamically

--- app/src/main/java/com/example/app1/AdventureFragment.java
+++ app/src/main/java/com/example/app1/AdventureFragment.java
@@ -34,7 +34,42 @@ public class AdventureFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+    public ConstraintLayout createNewTabElem(String name){
 
+        ConstraintLayout rec = new ConstraintLayout(requireContext());
+        rec.setId(View.generateViewId());
+        rec.setLayoutParams(new ConstraintLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                160
+        ));
+
+        TextView tv_rec = new TextView(requireContext());
+        tv_rec.setId(View.generateViewId());
+        tv_rec.setText(name);
+        tv_rec.setTextColor(Color.WHITE);
+        rec.addView(tv_rec);
+
+        rec.setBackgroundColor(Color.CYAN);
+
+        ConstraintSet res_set =  new ConstraintSet();
+        res_set.clone(rec);
+
+        res_set.connect(
+                tv_rec.getId(), ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 0
+        );
+        res_set.connect(
+                tv_rec.getId(), ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 0
+        );
+        res_set.connect(
+                tv_rec.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0
+        );
+        res_set.connect(
+                tv_rec.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0
+        );
+
+        res_set.applyTo(rec);
+        return rec;
+    }
 
     public void init(View v) {
 
@@ -60,25 +95,13 @@ public class AdventureFragment extends Fragment {
 
         ConstraintLayout par = v.findViewById(R.id.adv_list_parent);
 
-
-
-        ConstraintLayout rec = new ConstraintLayout(requireContext());
-        rec.setId(View.generateViewId());
-        rec.setLayoutParams(new ConstraintLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                160
-        ));
-
-        TextView tv_rec = new TextView(requireContext());
-        tv_rec.setId(View.generateViewId());
-        tv_rec.setText("Location Name");
-        tv_rec.setTextColor(Color.WHITE);
-
-
-        rec.setBackgroundColor(Color.CYAN);
+        ConstraintLayout rec = createNewTabElem("test");
+        ConstraintLayout rec2 = createNewTabElem("test2");
+        par.addView(rec);
+        par.addView(rec2);
 
         ConstraintSet res_set =  new ConstraintSet();
-        res_set.clone(rec);
+        res_set.clone(par);
 
         res_set.connect(
                 rec.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0
@@ -90,23 +113,18 @@ public class AdventureFragment extends Fragment {
                 rec.getId(), ConstraintSet.TOP, R.id.list_elem_1, ConstraintSet.BOTTOM, 0
         );
 
-
         res_set.connect(
-                tv_rec.getId(), ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 0
+                rec2.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0
         );
         res_set.connect(
-                tv_rec.getId(), ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 0
+                rec2.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0
         );
         res_set.connect(
-                tv_rec.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0
-        );
-        res_set.connect(
-                tv_rec.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0
+                rec2.getId(), ConstraintSet.TOP, rec.getId(), ConstraintSet.BOTTOM, 0
         );
 
-        res_set.applyTo(rec);
-        rec.addView(tv_rec);
-        par.addView(rec);
+        res_set.applyTo(par);
+
 
 
 //        TextView tv_rec = new TextView(requireContext());
